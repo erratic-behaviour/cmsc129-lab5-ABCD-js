1aeadd3605360d5a4a9d1a91c55bca064247bfbd
Sesilyo <c.sethleander@gmail.com>
2026-03-24 12:26:48 +0800

    added comments on script file

--- lab5_scripts.js
+++ lab5_scripts.js
@@ -1,9 +1,12 @@
 const time_now = () => {
+  // get HTML elements by ID block
   const dateElement = document.getElementById("currentDate");
   const timeElement = document.getElementById("currentTime");
 
+  // initializes new Date object as a constant
   const today = new Date();
 
+  // SHOW DATE block
   dateElement.textContent =
     "Today is " +
     today.toLocaleDateString("en-US", {
@@ -17,6 +20,7 @@ const time_now = () => {
     }) +
     ".";
 
+  // SHOW TIME block
   timeElement.textContent =
     "The current time is " +
     today.toLocaleTimeString("en-us", {
@@ -36,12 +40,15 @@ function generate_id() {
 }
 
 function add_student() {
+  // get user input from forms in HTML
   const name = document.getElementById("name").value;
   const age = Number(document.getElementById("age").value);
   const email = document.getElementById("email").value;
   const course = document.getElementById("courses").value;
   const studentNum = generate_id();
 
+
+  // validate if form is filled
   if (validate(name, age, email)) {
     let newStudent = {
       name: name,
@@ -51,10 +58,12 @@ function add_student() {
       course: course,
     };
 
+    // appends new student into array
     students.push(newStudent);
     takenIDs.push(studentNum);
     alert("Student added successfully!");
 
+    // resets the form input fields
     document.getElementById("name").value = "";
     document.getElementById("age").value = "";
     document.getElementById("email").value = "";
@@ -62,36 +71,46 @@ function add_student() {
   }
 }
 
+// arrays for storing students & their IDs
 const students = [];
 const takenIDs = [];
 
+// function to validate the form input fields
 function validate(name, age, email) {
   const regex = /^[^\s]+\s[^\s]+$/;
 
+  // name must be at least 5 characters long
   if (!regex.test(name) || name.length <= 5) {
     alert("Invalid name!");
     return false;
   }
 
+  // age cap: min = 18, max = 99
   if (age <= 18 || age > 99 || typeof age != "number") {
     alert("Invalid age!");
     return false;
   }
 
+  // email must be university provided
   if (!email.endsWith("@up.edu.ph")) {
     alert("Invalid email!");
     return false;
   }
+
+  // if all cases met, accept
   return true;
 }
 
+
+
 function find_student() {
-  const queriedStudent = document.getElementById("searchInput").value;
-  const found = students.find((n) => n.studentNum === queriedStudent);
+  const queriedStudent = document.getElementById("searchInput").value;  // initialize and get input from search field
+  const found = students.find((n) => n.studentNum === queriedStudent);  // if student found, proceed to display
   const displayStudent = document.getElementById("studentDisplay");
 
   console.log(found);
 
+  // displays table for student data
   if (found) {
     displayStudent.innerHTML = `
     <p>Student Found!</p>
@@ -115,15 +134,21 @@ function find_student() {
 }
 
 function display_list() {
-  const studentList = document.getElementById("studentList");
-  const tableBody = document.getElementById("directoryBody");
-  const emptyFallback = document.getElementById("emptyDirectory");
+  const studentList = document.getElementById("studentList");       // get table from HTML
+  const tableBody = document.getElementById("directoryBody");       // get table body from HTML
+  const emptyFallback = document.getElementById("emptyDirectory");  // if "database" empty
 
+  // "database" empty
   if (students.length == 0) {
     emptyFallback.hidden = false;
-  } else {
-    emptyFallback.hidden = true;
-    studentList.hidden = false;
+  }
+  
+  // if "database" not empty
+  else {
+    emptyFallback.hidden = true;  // hide empty display
+    studentList.hidden = false;   // display tables
+    
+    // display students on table
     tableBody.innerHTML = students
       .map(
         (student) => `
